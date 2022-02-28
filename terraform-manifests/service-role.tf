4d6c1c1d397e317b990820cee7e36bbc2ed52e17
rahul <86480235+kuntrapakam@users.noreply.github.com>
2022-02-28 12:03:52 +0530

    Update service-role.tf

--- terraform-manifests/service-role.tf
+++ terraform-manifests/service-role.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_service_linked_role" "autoscaling" {
   aws_service_name = "autoscaling.amazonaws.com"
   description      = "this is the rule"
-  custom_suffix    = "something-new-man"
+  custom_suffix    = "${random_pet.server.id}-something-new-man"
 
   # provisioner "local-exec" {
   #     command = "Start-Sleep -s 5"
